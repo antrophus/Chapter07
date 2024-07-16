e7f3c79015794e7345df59d4f6395b3389a18d99
antrophus <antrophus@yahoo.com>
2024-07-16 16:39:36 +0900

    7/16 소켓 통신 다중 접속 코드 작성중

--- src/echo/Server.java
+++ src/echo/Server.java
@@ -22,47 +22,29 @@ public class Server {
 		// 서버시작
 		System.out.println("<서버시작>");
 		System.out.println("============================================");
-		System.out.println("연결을 기다리고 있습니다.");
 
-		// socket은 종이컵 전화기 : 대기상태 ->클라이언트가 오면 serverSocket.에서 new socket을 빼서 만들어준다.
-		Socket socket = serverSocket.accept();
-		System.out.println("클라이언트가 연결 되었습니다.");
-
-		// 읽기 스트림 - 메세지 받기
-		InputStream in = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-
-		// 쓰기 스트림 - 메세지 보내기
-		OutputStream out = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-
+		// 반복
 		while (true) {
-			// 메세지 받기
-			String msg = br.readLine();
 
-			if (msg == null) {
-				break;
-			}
+			System.out.println("연결을 기다리고 있습니다.");
+			// 클라이언트가 오면 serverSocket.에서 new socket을 빼서 만들어준다.
+			// socket은 종이컵 전화기 : 대기상태
+			Socket socket = serverSocket.accept();
 
-			System.out.println("받은 메세지: " + msg);
-
-			// 메세지 보내기
-			bw.write(msg);
-			bw.newLine();
-			bw.flush();
+			Thread thread = new ServerThread(socket);
+			thread.start();
 
 		}
-
-		System.out.println("============================================");
-		System.out.println("서버 종료");
-
-		// 닫기
-		br.close();
-		bw.close();
-		socket.close();
-		serverSocket.close();
+		/*
+		 * System.out.println("============================================");
+		 * System.out.println("서버 종료");
+		 * 
+		 * // 닫기
+		 * br.close(); 
+		 * bw.close(); 
+		 * socket.close(); 
+		 * serverSocket.close();
+		 */
 
 	}
 
